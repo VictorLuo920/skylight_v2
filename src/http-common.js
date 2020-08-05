1896216d38a92d3abfa936bbfc527ec94fa67112
Victor Luo <victorluo920@gmail.com>
2020-08-05 15:19:43 -0700

    Updates

--- src/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://skylight-react-interview-project.s3.amazonaws.com",
-  headers: {
-    "Content-type": "application/json",
-  },
-});
